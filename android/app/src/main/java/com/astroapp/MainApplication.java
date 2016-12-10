5c53db3b49e6900c5af36bde11f635c004ebad35
Gabriel Rubens <grsabreu@gmail.com>
2016-12-09 22:01:09 -0200

    Upgrade react, react-native and all deps

--- android/app/src/main/java/com/astroapp/MainApplication.java
+++ android/app/src/main/java/com/astroapp/MainApplication.java
@@ -4,13 +4,14 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.react.rnspinkit.RNSpinkitPackage;
+import cl.json.RNSharePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,16 +27,22 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new RNSpinkitPackage(),
-           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNSpinkitPackage()
+          new MainReactPackage(),
+            new RNSpinkitPackage(),
+            new RNSharePackage(),
+            new VectorIconsPackage()
       );
     }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
