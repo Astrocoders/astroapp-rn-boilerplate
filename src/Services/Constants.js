7b54f13c3bbdff7e2b94a64bf10d421703aa0605
Gabriel Rubens <grsabreu@gmail.com>
2017-02-19 19:29:51 -0300

    Update constants

--- src/Services/Constants.js
+++ src/Services/Constants.js
@@ -1,6 +1,4 @@
 export default {
-  METEOR_SERVER: 'ws://127.0.0.1:3000/websocket',
-  CLOUDINARY_API_SECRET: 'abcfalsjfal',
-  CLOUDINARY_API_KEY: '000000000000000',
-  CLOUDINARY_CLOUD: 'astrosomething',
+  IS_APOLLO_APP: false,
+  IS_METEOR_APP: true,
 };
