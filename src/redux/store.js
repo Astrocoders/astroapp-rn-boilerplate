50fa676f9b3f1152871d9456a94b93eef9050f8b
Gabriel Rubens <grsabreu@gmail.com>
2017-03-20 21:40:09 -0300

    Update redux/store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,41 @@
-import { 
-  createStore,
-  applyMiddleware,
-} from 'redux'
-import createSagaMiddleware from 'redux-saga'
-import createLogger from 'redux-logger'
-import reducers from './reducers'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { compact } from 'lodash'
+import { createLogicMiddleware } from 'redux-logic'
+import { AsyncStorage } from 'react-native'
+import {
+  persistStore,
+  autoRehydrate,
+} from 'redux-persist'
+
+import rootReducers from './reducers'
+import rootLogics from './logics'
+
+const logicMiddleware = createLogicMiddleware(rootLogics)
+
+const middlewares = [
+  logicMiddleware,
+]
+
+const devToolsEnhancer = __DEV__ ? require('remote-redux-devtools') : () => ()
+const enhancer = compose(...compact([
+  applyMiddleware(...middlewares),
+  autoRehydrate(),
+  __DEV__ ? devToolsEnhancer({ realtime: true }) : null,
+]))
 
 const store = createStore(
-  reducers,
-  applyMiddleware(
-    createSagaMiddleware(),
-    createLogger({
-      predicate: () => __DEV__,
-    })
-  )
+  rootReducers,
+  enhancer,
 )
+
+if (module.hot) {
+  // Enable hot module replacement for reducers
+  module.hot.accept(() => {
+    const nextRootReducer = require('./reducers').default
+    store.replaceReducer(nextRootReducer)
+  })
+}
+
+const persistor = persistStore(store, { storage: AsyncStorage })
 
 export default store
