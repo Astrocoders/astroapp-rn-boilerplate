ee737d2c27388feb1f8510c6a2eb73c042571e71
Gabriel Rubens <grsabreu@gmail.com>
2017-03-29 23:59:50 -0300

    Setup reduxForm for immutable

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux-immutable'
-import { reducer as form } from 'redux-form'
+import { reducer as form } from 'redux-form/immutable'
 import app from './App/reducer'
 
 export default combineReducers({
