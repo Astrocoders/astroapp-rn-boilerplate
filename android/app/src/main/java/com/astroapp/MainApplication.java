7706045405b93efba4e57305aeefb53802bbd362
Gabriel Rubens <grsabreu@gmail.com>
2017-04-04 11:38:51 -0300

    Upgrade react-native files

--- android/app/src/main/java/com/astroapp/MainApplication.java
+++ android/app/src/main/java/com/astroapp/MainApplication.java
@@ -1,12 +1,8 @@
 package com.astroapp;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -19,16 +15,14 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNSpinkitPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
   };
