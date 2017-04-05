226e6a0049b036f9c579ff9bf8a298a1e9f69721
Gabriel Rubens <grsabreu@gmail.com>
2017-04-05 16:07:30 -0300

    Update ContentOrLoader

--- src/Components/ContentOrLoader.js
+++ src/Components/ContentOrLoader.js
@@ -1,12 +1,13 @@
-import Loader from './Loader'
+import { Spinner } from 'native-base'
 import Nothing from './Nothing'
 import React from 'react'
+import theme from '~/theme'
 import {
   isBoolean,
 } from 'lodash'
 
 export default props => {
-  if(props.isLoading || !isBoolean(props.isLoading)) return <Loader/>
+  if(props.isLoading || !isBoolean(props.isLoading)) return <Spinner color={theme.colors.blue}/>
   if(props.isNothing) return <Nothing message={props.nothingMessage}/>
   return props.children
 }
